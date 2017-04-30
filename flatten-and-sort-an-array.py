8160a8388bee4471330e0ba6ab8b6c15df89e1fd
Sergey <sabarovsky@yahoo.com>
2017-04-30 23:15:09 +0300

    starting

---
+++ flatten-and-sort-an-array.py
@@ -0,0 +1,4 @@
+def flatten_and_sort(array):
+    array = sum(array, [])
+    array = sorted(array)
+    return array
---
+++ square-n-sum.py
@@ -0,0 +1,5 @@
+def square_sum(numbers):
+    rezult = 0
+    for i in numbers:
+        rezult+=i*i
+    return rezult
