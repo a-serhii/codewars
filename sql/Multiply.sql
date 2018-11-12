426887b414ee72e7c37c0bc2b0efd90daba0110a
serhii <aserhii@protonmail.com>
2018-11-12 16:09:37 +0200

    Add sql task

---
+++ sql/Multiply.sql
@@ -0,0 +1,4 @@
+/* PostgreSQL 9.6 */
+/* https://www.codewars.com/kata/multiply/train/sql */
+
+SELECT price * amount AS total FROM items;
---
+++ sql/adults-only-sql-for-beginners-number-1.sql
@@ -0,0 +1,3 @@
+--https://www.codewars.com/kata/adults-only-sql-for-beginners-number-1/train/sql
+
+SELECT name, age FROM users WHERE age >= 18;
